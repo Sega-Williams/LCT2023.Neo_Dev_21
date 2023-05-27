fdd83845b7f7060015b6cada49ec8d401d4801a9
Maria20Maria20 <mari.avi00@mail.ru>
2023-05-27 20:19:18 +0400

    Add asset
    1. Добавила ассет мск
    2. Добавила новую тестовую сцену
    3. Удалила цезиум

---
+++ Assets/BotMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BotMovement : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+    private Transform[] _pathPoints;
+    private int _index = 0;
+    private float _minDistance = 10;
+    bool stopMove;
+
+    [SerializeField] GameObject path;
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+
+        _pathPoints = new Transform[path.transform.childCount];
+        for (int i = 0; i < _pathPoints.Length; i++)
+        {
+            _pathPoints[i] = path.transform.GetChild(i);
+        }
+    }
+
+    private void Update()
+    {
+        Walk();
+    }
+
+    void Walk()
+    {
+        if (!stopMove)
+        {
+            if (Vector3.Distance(transform.position, _pathPoints[_index].position) < _minDistance)
+            {
+                if (_index + 1 != _pathPoints.Length)
+                {
+                    _index++;
+                }
+                else
+                {
+                    stopMove = true;
+                }
+            }
+            _agent.SetDestination(_pathPoints[_index].position);
+        }
+    }
+
+}
